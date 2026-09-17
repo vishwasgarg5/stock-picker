478ae836b82a625d0017c4198c349455685d2ff2
Vishwas Garg <vishwasgarg.ml@gmail.com>
2026-09-17 22:38:55 +0530

    feat: run learned paper trading in evening job

--- jobs/evening.py
+++ jobs/evening.py
@@ -6,7 +6,9 @@ if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
 from src.pipeline import run_evening
+from src.paper_trading import run_paper_trading
 
 
 if __name__ == "__main__":
     run_evening()
+    run_paper_trading()
